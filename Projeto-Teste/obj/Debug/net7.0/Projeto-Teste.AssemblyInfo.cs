1f99ccad2a1d8dba3ad82b8b12c10e022211f1dd
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2025-03-11 18:02:26 -0300

    new

--- Projeto-Teste/obj/Debug/net7.0/Projeto-Teste.AssemblyInfo.cs
+++ Projeto-Teste/obj/Debug/net7.0/Projeto-Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cf4c67aaa8f412b5276e171fc0d5dc734219c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
